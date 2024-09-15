f15ee35e1ca718ac533841f980c727020cf90d0f
KevinHendersonCSUF <123698269+KevinHendersonCSUF@users.noreply.github.com>
2024-09-15 23:21:16 +0000

    Started code

--- Handout2.cpp
+++ Handout2.cpp
@@ -8,9 +8,17 @@
 #include<iostream>
 #include<string>
 #include<stack>
+#include<cstdlib>
 
 int main() {
+std::string expression;
 
-
+while(true) {
+std::stack<int> stack;
+std::cout << "Enter a postfix expression with a $ at the end:" << std::endl;
+std::cin >> expression;
+for(int i = 0; i < expression.size(), i++;) {
+}
+}
 return 0;
 }
